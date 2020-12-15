72dd772ec3b6c1600d6bc1dab0851d481202b387
Charith Munasinghe <eranga.charith@gmail.com>
2020-12-15 22:27:00 +0100

    transforms are working now

--- src/static_transforms.cpp
+++ src/static_transforms.cpp
@@ -1,36 +1,41 @@
+
 #include <geometry_msgs/TransformStamped.h>
 #include <ros/ros.h>
 #include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf_conversions/tf_eigen.h>
 
 #include <Eigen/Dense>
 
-geometry_msgs::TransformStamped publish_static_transform(std::string child_frame,
-                                                         Eigen::Matrix<double, 4, 4, Eigen::RowMajor> &mat);
+geometry_msgs::TransformStamped get_static_transform(std::string parent_frame, std::string child_frame,
+                                                     Eigen::Matrix<double, 4, 4, Eigen::RowMajor> &mat);
 
 int main(int argc, char **argv)
 {
-  /*
-   * INITIALIZE ROS NODE
-   */
+  // init node
   ros::init(argc, argv, "static_transforms");
   ros::NodeHandle nh;
   ros::NodeHandle priv_nh_("~");
 
-  /*
-   * SET UP PARAMETERS (COULD BE INPUT FROM LAUNCH FILE/TERMINAL)
-   */
+  // Setup Parameters
   std::vector<double> trans12_param, trans13_param, trans14_param;
 
-  if (!priv_nh_.getParam("/sensor12_transform", trans12_param))
-    ROS_ERROR("Failed to get parameter sensor12_transform from server.");
+  if (!priv_nh_.getParam("sensor12_transform", trans12_param))
+    ROS_ERROR("Node: static_transforms:: Failed to get parameter sensor12_transform from server.");
 
-  if (!priv_nh_.getParam("/sensor13_transform", trans13_param))
-    ROS_ERROR("Failed to get parameter sensor13_transform from server.");
+  if (!priv_nh_.getParam("sensor13_transform", trans13_param))
+    ROS_ERROR("Node: static_transforms:: Failed to get parameter sensor13_transform from server.");
 
-  if (!priv_nh_.getParam("/sensor14_transform", trans14_param))
-    ROS_ERROR("Failed to get parameter sensor14_transform from server.");
+  if (!priv_nh_.getParam("sensor14_transform", trans14_param))
+    ROS_ERROR("Node: static_transforms:: Failed to get parameter sensor14_transform from server.");
+
+  // Populate transform matrices from parameters
+  Eigen::Matrix<double, 4, 4, Eigen::RowMajor> trans12(trans12_param.data());
+  Eigen::Matrix<double, 4, 4, Eigen::RowMajor> trans13(trans13_param.data());
+  Eigen::Matrix<double, 4, 4, Eigen::RowMajor> trans14(trans14_param.data());
+
+  std::cout << trans12 << std::endl << trans13 << std::endl << trans14 << std::endl;
 
   /*
    * SETUP PUBLISHERS
@@ -42,56 +47,48 @@ int main(int argc, char **argv)
   // sensor3_transform = nh.advertise<geometry_msgs::TransformStamped>("env/transform/sensor3", 1, true);
   // sensor4_transform = nh.advertise<geometry_msgs::TransformStamped>("env/transform/sensor4", 1, true);
 
-  /*
-   * populate the transform matrices
-   */
-  Eigen::Matrix<double, 4, 4, Eigen::RowMajor> trans12(trans12_param.data()), trans13(trans13_param.data()),
-      trans14(trans14_param.data());
-
-  std::cout << trans12 << std::endl << trans13 << std::endl << trans14 << std::endl;
-
-  tf2_ros::TransformBroadcaster transform_broadcaster;
+  // tf2_ros::TransformBroadcaster transform_broadcaster;
+  static tf2_ros::StaticTransformBroadcaster static_transform_broadcaster;
 
   geometry_msgs::TransformStamped sensor12_transformStamped, sensor13_transformStamped, sensor14_transformStamped;
 
-  // ros::Rate loop_rate(10);
+  // ros::Rate rate(10.0);
 
   // while (ros::ok())
   // {
+
   //   sensor1_transform.publish(sensor12_transformStamped);
   //   sensor2_transform.publish(sensor13_transformStamped);
   //   sensor3_transform.publish(sensor14_transformStamped);
 
-  //   loop_rate.sleep();
+  //   const std::vector<geometry_msgs::TransformStamped> alltransforms{ sensor12_transformStamped,
+  //   sensor13_transformStamped,
+  //   sensor14_transformStamped };
+  //   transform_broadcaster.sendTransform(sensor12_transformStamped);
+  //   rate.sleep();
   // }
 
-  ros::Rate rate(10.0);
+  sensor12_transformStamped = get_static_transform("sensor1_frame", "sensor2_frame", trans12);
+  sensor13_transformStamped = get_static_transform("sensor1_frame", "sensor3_frame", trans13);
+  sensor14_transformStamped = get_static_transform("sensor1_frame", "sensor4_frame", trans14);
 
-  while (ros::ok())
-  {
-    sensor12_transformStamped = publish_static_transform("sensor2_frame", trans12);
-    // sensor13_transformStamped = publish_static_transform("sensor3_frame", trans13);
-    // sensor14_transformStamped = publish_static_transform("sensor4_frame", trans14);
+  const std::vector<geometry_msgs::TransformStamped> alltransforms{ sensor12_transformStamped,
+                                                                    sensor13_transformStamped,
+                                                                    sensor14_transformStamped };
+  static_transform_broadcaster.sendTransform(alltransforms);
 
-    //const std::vector<geometry_msgs::TransformStamped> alltransforms{ sensor12_transformStamped,
-                                                                      // sensor13_transformStamped,
-                                                                      // sensor14_transformStamped };
-    transform_broadcaster.sendTransform(sensor12_transformStamped);
-    rate.sleep();
-  }
-
-  // ros::spin();
+  ros::spin();
 
   return 0;
 }
 
-geometry_msgs::TransformStamped publish_static_transform(std::string child_frame,
-                                                         Eigen::Matrix<double, 4, 4, Eigen::RowMajor> &mat)
+geometry_msgs::TransformStamped get_static_transform(std::string parent_frame, std::string child_frame,
+                                                     Eigen::Matrix<double, 4, 4, Eigen::RowMajor> &mat)
 {
   geometry_msgs::TransformStamped stat_trans;
 
   stat_trans.header.stamp = ros::Time::now();
-  stat_trans.header.frame_id = "sensor1_frame";
+  stat_trans.header.frame_id = parent_frame;
   stat_trans.child_frame_id = child_frame;
 
   stat_trans.transform.translation.x = mat(0, 3);
@@ -110,7 +107,7 @@ geometry_msgs::TransformStamped publish_static_transform(std::string child_frame
   stat_trans.transform.rotation.z = q.z();
   stat_trans.transform.rotation.w = q.w();
 
-  ROS_INFO("Publishing transformation: %s to world", child_frame.c_str());
+  ROS_INFO("Publishing transformation: %s to %s", child_frame.c_str(), parent_frame.c_str());
 
   return stat_trans;
 }
--- src/transformpipe_node.cpp
+++ src/transformpipe_node.cpp
@@ -11,157 +11,148 @@
 class CloudTransformer
 {
 public:
-  CloudTransformer(ros::NodeHandle &nh, ros::NodeHandle &priv_nh_)
-  {
-    tfListener1 = new tf2_ros::TransformListener(tfBuffer1);
-    // tfListener2 = new tf2_ros::TransformListener(tfBuffer2);
-    // tfListener3 = new tf2_ros::TransformListener(tfBuffer3);
-    // tfListener4 = new tf2_ros::TransformListener(tfBuffer4);
+  CloudTransformer(ros::NodeHandle &nh, ros::NodeHandle &priv_nh_);
 
-    world_frame = priv_nh_.param<std::string>("world_frame", "world_frame");
+  void run(ros::NodeHandle &nh);
 
-    sensor1_frame = priv_nh_.param<std::string>("sensor1_frame", "sensor1_frame");
-    sensor2_frame = priv_nh_.param<std::string>("sensor2_frame", "sensor2_frame");
-    sensor3_frame = priv_nh_.param<std::string>("sensor3_frame", "sensor3_frame");
-    sensor4_frame = priv_nh_.param<std::string>("sensor4_frame", "sensor4_frame");
-
-    sensor_frames = { sensor1_frame, sensor2_frame, sensor3_frame, sensor4_frame };
-
-    sensor1_topic = priv_nh_.param<std::string>("sensor1_topic", "env/sensor1/points");
-    sensor2_topic = priv_nh_.param<std::string>("sensor2_topic", "env/sensor2/points");
-    sensor3_topic = priv_nh_.param<std::string>("sensor3_topic", "env/sensor3/points");
-    sensor4_topic = priv_nh_.param<std::string>("sensor4_topic", "env/sensor4/points");
-
-    sensor_topics = { sensor1_topic, sensor2_topic, sensor3_topic, sensor4_topic };
-
-    sensor1_pub = nh.advertise<sensor_msgs::PointCloud2>("env/sensor1/alignedpoints", 1);
-    sensor2_pub = nh.advertise<sensor_msgs::PointCloud2>("env/sensor2/alignedpoints", 1);
-    sensor3_pub = nh.advertise<sensor_msgs::PointCloud2>("env/sensor3/alignedpoints", 1);
-    sensor4_pub = nh.advertise<sensor_msgs::PointCloud2>("env/sensor4/alignedpoints", 1);
-
-    combined_pub = nh.advertise<sensor_msgs::PointCloud2>("env/combinedpoints", 1);
-  }
-
-  void run(ros::NodeHandle &nh)
-  {
-    while (ros::ok())
-    {
-      /*
-       * LISTEN FOR POINTCLOUD
-       */
-      std::string sensor1topic = nh.resolveName(sensor1_topic);
-      std::string sensor2topic = nh.resolveName(sensor2_topic);
-      std::string sensor3topic = nh.resolveName(sensor3_topic);
-      std::string sensor4topic = nh.resolveName(sensor4_topic);
-
-      ROS_INFO_STREAM("Cloud service called; waiting for a PointCloud2 on topics " << std::endl
-                                                                                   << sensor1_topic << std::endl
-                                                                                   << sensor2_topic << std::endl
-                                                                                   << sensor3_topic << std::endl
-                                                                                   << sensor4_topic);
-
-      sensor_msgs::PointCloud2::ConstPtr raw_cloud1 =
-          ros::topic::waitForMessage<sensor_msgs::PointCloud2>(sensor1topic, nh);
-      sensor_msgs::PointCloud2::ConstPtr raw_cloud2 =
-          ros::topic::waitForMessage<sensor_msgs::PointCloud2>(sensor2topic, nh);
-      sensor_msgs::PointCloud2::ConstPtr raw_cloud3 =
-          ros::topic::waitForMessage<sensor_msgs::PointCloud2>(sensor3topic, nh);
-
-      // set to also pointcloud 3 for testing purposes
-      // sensor_msgs::PointCloud2::ConstPtr raw_cloud4 =
-      //     ros::topic::waitForMessage<sensor_msgs::PointCloud2>(sensor3topic, nh);
-
-      /*
-       * TRANSFORM POINTCLOUD FROM CAMERA FRAME TO WORLD FRAME
-       */
-
-      geometry_msgs::TransformStamped transformStamped1, transformStamped2, transformStamped3, transformStamped4,
-          transformStamped5;
-
-      // ROS_INFO_STREAM("Cloud service called; waiting for a transforms on topics env/transform/sensor1,2,3 " << std::endl);
-
-      // geometry_msgs::TransformStamped transformStamped1 =
-      //     *(ros::topic::waitForMessage<geometry_msgs::TransformStamped>("env/transform/sensor1", nh));
-
-      // geometry_msgs::TransformStamped transformStamped2 =
-      //     *(ros::topic::waitForMessage<geometry_msgs::TransformStamped>("env/transform/sensor2", nh));
-
-      // geometry_msgs::TransformStamped transformStamped3 =
-      //     *(ros::topic::waitForMessage<geometry_msgs::TransformStamped>("env/transform/sensor3", nh));
-
-      // geometry_msgs::TransformStamped transformStamped4 =
-      //     *(ros::topic::waitForMessage<geometry_msgs::TransformStamped>("env/transform/sensor4", nh));
-
-      try
-      {
-        transformStamped1 = tfBuffer1.lookupTransform(world_frame, sensor1_frame, ros::Time(0), ros::Duration(1));
-        transformStamped2 = tfBuffer1.lookupTransform(world_frame, sensor2_frame, ros::Time(0), ros::Duration(1));
-        // transformStamped3 = tfBuffer1.lookupTransform(world_frame, sensor3_frame, ros::Time(0), ros::Duration(1));
-        // transformStamped4 = tfBuffer1.lookupTransform(world_frame, sensor4_frame, ros::Time(0), ros::Duration(1));
-      }
-      catch (tf2::TransformException &ex)
-      {
-        ROS_WARN("%s", ex.what());
-        ros::Duration(1.0).sleep();
-        continue;
-      }
-
-      sensor_msgs::PointCloud2 transformed_cloud1, transformed_cloud2, transformed_cloud3, transformed_cloud4;
-
-      pcl_ros::transformPointCloud(world_frame, transformStamped1.transform, *raw_cloud1, transformed_cloud1);
-      pcl_ros::transformPointCloud(world_frame, transformStamped2.transform, *raw_cloud2, transformed_cloud2);
-      // pcl_ros::transformPointCloud(world_frame, transformStamped3.transform, *raw_cloud3, transformed_cloud3);
-      // pcl_ros::transformPointCloud(world_frame, transformStamped4.transform, *raw_cloud4, transformed_cloud4);
-
-      /*
-       * CONVERT POINTCLOUD ROS->PCL
-       */
-      // pcl::PointCloud<pcl::PointXYZ> cloud;
-      // pcl::fromROSMsg(transformed_cloud, cloud);
-
-      /*
-       * CONVERT POINTCLOUD PCL->ROS
-       */
-      // sensor_msgs::PointCloud2::Ptr pc2_cloud(new sensor_msgs::PointCloud2);
-      // pcl::toROSMsg(*prism_filtered_cloud, *pc2_cloud);
-
-      // pc2_cloud->header.frame_id = world_frame;
-      // pc2_cloud->header.stamp = ros::Time::now();
-      // object_pub.publish(pc2_cloud);
-
-      sensor1_pub.publish(transformed_cloud1);
-      sensor2_pub.publish(transformed_cloud2);
-      // sensor3_pub.publish(transformed_cloud3);
-      // sensor4_pub.publish(transformed_cloud4);
-    }
-  }
+  geometry_msgs::TransformStamped identityTransform(std::string parent_frame, std::string child_frame);
 
 private:
-  /*
-   * SET UP PARAMETERS (COULD BE INPUT FROM LAUNCH FILE/TERMINAL)
-   */
+  // Parameters
   std::string world_frame;
   std::string sensor1_frame, sensor2_frame, sensor3_frame, sensor4_frame;
   std::string sensor1_topic, sensor2_topic, sensor3_topic, sensor4_topic;
   std::array<std::string, 4> sensor_frames, sensor_topics;
 
-  /*
-   * SETUP PUBLISHERS
-   */
+  // Publishers
   ros::Publisher sensor1_pub, sensor2_pub, sensor3_pub, sensor4_pub, combined_pub;
 
-  /*
-   * Static transform publishers
-   */
-  tf2_ros::Buffer tfBuffer1;                // tfBuffer2 tfBuffer3, tfBuffer4, tfBufferTest;
-  tf2_ros::TransformListener *tfListener1;  // *tfListener2, *tfListener3, *tfListener4, *tfListenerTest;
+  sensor_msgs::PointCloud2 transformed_cloud1, transformed_cloud2, transformed_cloud3, transformed_cloud4;
+
+  // Transform listner
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener *tfListener;
 };
 
+// Constructor
+CloudTransformer::CloudTransformer(ros::NodeHandle &nh, ros::NodeHandle &priv_nh_)
+{
+  tfListener = new tf2_ros::TransformListener(tfBuffer);
+
+  world_frame = priv_nh_.param<std::string>("world_frame", "world_frame");
+
+  sensor1_frame = priv_nh_.param<std::string>("sensor1_frame", "sensor1_frame");
+  sensor2_frame = priv_nh_.param<std::string>("sensor2_frame", "sensor2_frame");
+  sensor3_frame = priv_nh_.param<std::string>("sensor3_frame", "sensor3_frame");
+  sensor4_frame = priv_nh_.param<std::string>("sensor4_frame", "sensor4_frame");
+
+  sensor_frames = { sensor1_frame, sensor2_frame, sensor3_frame, sensor4_frame };
+
+  sensor1_topic = priv_nh_.param<std::string>("sensor1_topic", "env/sensor1/points");
+  sensor2_topic = priv_nh_.param<std::string>("sensor2_topic", "env/sensor2/points");
+  sensor3_topic = priv_nh_.param<std::string>("sensor3_topic", "env/sensor3/points");
+  sensor4_topic = priv_nh_.param<std::string>("sensor4_topic", "env/sensor4/points");
+
+  sensor_topics = { sensor1_topic, sensor2_topic, sensor3_topic, sensor4_topic };
+
+  sensor1_pub = nh.advertise<sensor_msgs::PointCloud2>("env/sensor1/alignedpoints", 1);
+  sensor2_pub = nh.advertise<sensor_msgs::PointCloud2>("env/sensor2/alignedpoints", 1);
+  sensor3_pub = nh.advertise<sensor_msgs::PointCloud2>("env/sensor3/alignedpoints", 1);
+  sensor4_pub = nh.advertise<sensor_msgs::PointCloud2>("env/sensor4/alignedpoints", 1);
+
+  combined_pub = nh.advertise<sensor_msgs::PointCloud2>("env/combinedpoints", 1);
+}
+
+// Main program
+void CloudTransformer::run(ros::NodeHandle &nh)
+{
+  while (ros::ok())
+  {
+    std::string sensor1topic = nh.resolveName(sensor1_topic);
+    std::string sensor2topic = nh.resolveName(sensor2_topic);
+    std::string sensor3topic = nh.resolveName(sensor3_topic);
+    std::string sensor4topic = nh.resolveName(sensor4_topic);
+
+    ROS_INFO_STREAM("Cloud service called; waiting for a PointCloud2 on topics " << std::endl
+                                                                                 << sensor1_topic << std::endl
+                                                                                 << sensor2_topic << std::endl
+                                                                                 << sensor3_topic << std::endl
+                                                                                 << sensor4_topic);
+
+    sensor_msgs::PointCloud2::ConstPtr raw_cloud1 =
+        ros::topic::waitForMessage<sensor_msgs::PointCloud2>(sensor1topic, nh);
+
+    sensor_msgs::PointCloud2::ConstPtr raw_cloud2 =
+        ros::topic::waitForMessage<sensor_msgs::PointCloud2>(sensor2topic, nh);
+
+    sensor_msgs::PointCloud2::ConstPtr raw_cloud3 =
+        ros::topic::waitForMessage<sensor_msgs::PointCloud2>(sensor3topic, nh);
+
+    geometry_msgs::TransformStamped transformStamped1, transformStamped2, transformStamped3, transformStamped4,
+        transformStamped5;
+
+    // geometry_msgs::TransformStamped transformStamped1 =
+    //     *(ros::topic::waitForMessage<geometry_msgs::TransformStamped>("env/transform/sensor1", nh));
+
+    try
+    {
+      transformStamped2 = tfBuffer.lookupTransform(sensor1_frame, sensor2_frame, ros::Time(0), ros::Duration(1));
+      transformStamped3 = tfBuffer.lookupTransform(sensor1_frame, sensor3_frame, ros::Time(0), ros::Duration(1));
+    }
+    catch (tf2::TransformException &ex)
+    {
+      ROS_WARN("%s", ex.what());
+      ros::Duration(1.0).sleep();
+      continue;
+    }
+
+    transformStamped1 = identityTransform("sensor1_frame", "sensor1_frame");
+
+    pcl_ros::transformPointCloud(world_frame, transformStamped1.transform, *raw_cloud1, transformed_cloud1);
+    pcl_ros::transformPointCloud(world_frame, transformStamped2.transform, *raw_cloud2, transformed_cloud2);
+    pcl_ros::transformPointCloud(world_frame, transformStamped3.transform, *raw_cloud3, transformed_cloud3);
+
+    sensor1_pub.publish(transformed_cloud1);
+    sensor2_pub.publish(transformed_cloud2);
+    sensor3_pub.publish(transformed_cloud3);
+  }
+}
+
+// Identity-transform creator
+geometry_msgs::TransformStamped CloudTransformer::identityTransform(std::string parent_frame, std::string child_frame)
+{
+  Eigen::Matrix<double, 4, 4, Eigen::RowMajor> mat = Eigen::Matrix4d::Identity();
+  Eigen::Matrix3d rot;
+
+  geometry_msgs::TransformStamped empty_transform;
+
+  empty_transform.header.stamp = ros::Time::now();
+  empty_transform.header.frame_id = parent_frame;
+  empty_transform.child_frame_id = child_frame;
+
+  empty_transform.transform.translation.x = mat(0, 3);
+  empty_transform.transform.translation.y = mat(1, 3);
+  empty_transform.transform.translation.z = mat(2, 3);
+
+  rot = mat.block<3, 3>(0, 0);
+
+  const Eigen::Quaterniond q(rot);
+
+  empty_transform.transform.rotation.x = q.x();
+  empty_transform.transform.rotation.y = q.y();
+  empty_transform.transform.rotation.z = q.z();
+  empty_transform.transform.rotation.w = q.w();
+
+  std::cout << "rot: " << rot << std::endl << q.x() << " " << q.y() << " " << q.z() << " " << std::endl;
+
+  ROS_INFO("transformation: %s to world", child_frame.c_str());
+
+  return empty_transform;
+}
+
+// main
 int main(int argc, char *argv[])
 {
-  /*
-   * INITIALIZE ROS NODE
-   */
   ros::init(argc, argv, "transformpipe_node");
 
   ros::NodeHandle nh;
