92634f8faa71483a7b0df3f786b2f1e334a2bd03
Charith Munasinghe <eranga.charith@gmail.com>
2021-03-06 21:01:18 +0100

    fix world transform

--- src/perception_node.cpp
+++ src/perception_node.cpp
@@ -63,136 +63,6 @@ ros::Publisher octreefilter_pub, voxelfilter_pub, passfilter_pub;
 // last frame time
 ros::Time lastSavedTimeStamp;
 
-void transformAsMatrix(const geometry_msgs::Transform &bt,
-                       Eigen::Matrix4f &out_mat)
-{
-  out_mat = tf2::transformToEigen(bt).matrix().cast<float>();
-}
-
-void transformPointCloud(const Eigen::Matrix4f &transform,
-                         const sensor_msgs::PointCloud2 &in,
-                         sensor_msgs::PointCloud2 &out)
-{
-  // Get X-Y-Z indices
-  int x_idx = pcl::getFieldIndex(in, "x");
-  int y_idx = pcl::getFieldIndex(in, "y");
-  int z_idx = pcl::getFieldIndex(in, "z");
-
-  if (x_idx == -1 || y_idx == -1 || z_idx == -1)
-  {
-    ROS_ERROR("Input dataset has no X-Y-Z coordinates! Cannot convert to Eigen format.");
-    return;
-  }
-
-  if (in.fields[x_idx].datatype != sensor_msgs::PointField::FLOAT32 ||
-      in.fields[y_idx].datatype != sensor_msgs::PointField::FLOAT32 ||
-      in.fields[z_idx].datatype != sensor_msgs::PointField::FLOAT32)
-  {
-    ROS_ERROR("X-Y-Z coordinates not floats. Currently only floats are supported.");
-    return;
-  }
-
-  // Check if distance is available
-  int dist_idx = pcl::getFieldIndex(in, "distance");
-
-  // Copy the other data
-  if (&in != &out)
-  {
-    out.header = in.header;
-    out.height = in.height;
-    out.width = in.width;
-    out.fields = in.fields;
-    out.is_bigendian = in.is_bigendian;
-    out.point_step = in.point_step;
-    out.row_step = in.row_step;
-    out.is_dense = in.is_dense;
-    out.data.resize(in.data.size());
-    // Copy everything as it's faster than copying individual elements
-    memcpy(&out.data[0], &in.data[0], in.data.size());
-  }
-
-  Eigen::Array4i xyz_offset(in.fields[x_idx].offset, in.fields[y_idx].offset, in.fields[z_idx].offset, 0);
-
-  for (size_t i = 0; i < in.width * in.height; ++i)
-  {
-    Eigen::Vector4f pt(*(float *)&in.data[xyz_offset[0]], *(float *)&in.data[xyz_offset[1]], *(float *)&in.data[xyz_offset[2]], 1);
-    Eigen::Vector4f pt_out;
-
-    bool max_range_point = false;
-    int distance_ptr_offset = (dist_idx < 0 ? -1 : (i * in.point_step + in.fields[dist_idx].offset)); // If dist_idx is negative, it must not be used as an index
-    float *distance_ptr = (dist_idx < 0 ? NULL : (float *)(&in.data[distance_ptr_offset]));
-    if (!std::isfinite(pt[0]) || !std::isfinite(pt[1]) || !std::isfinite(pt[2]))
-    {
-      if (distance_ptr == NULL || !std::isfinite(*distance_ptr)) // Invalid point
-      {
-        pt_out = pt;
-      }
-      else // max range point
-      {
-        pt[0] = *distance_ptr; // Replace x with the x value saved in distance
-        pt_out = transform * pt;
-        max_range_point = true;
-        //std::cout << pt[0]<<","<<pt[1]<<","<<pt[2]<<" => "<<pt_out[0]<<","<<pt_out[1]<<","<<pt_out[2]<<"\n";
-      }
-    }
-    else
-    {
-      pt_out = transform * pt;
-    }
-
-    if (max_range_point)
-    {
-      // Save x value in distance again
-      *(float *)(&out.data[distance_ptr_offset]) = pt_out[0];
-      pt_out[0] = std::numeric_limits<float>::quiet_NaN();
-    }
-
-    memcpy(&out.data[xyz_offset[0]], &pt_out[0], sizeof(float));
-    memcpy(&out.data[xyz_offset[1]], &pt_out[1], sizeof(float));
-    memcpy(&out.data[xyz_offset[2]], &pt_out[2], sizeof(float));
-
-    xyz_offset += in.point_step;
-  }
-
-  // Check if the viewpoint information is present
-  int vp_idx = pcl::getFieldIndex(in, "vp_x");
-  if (vp_idx != -1)
-  {
-    // Transform the viewpoint info too
-    for (size_t i = 0; i < out.width * out.height; ++i)
-    {
-      float *pstep = (float *)&out.data[i * out.point_step + out.fields[vp_idx].offset];
-      // Assume vp_x, vp_y, vp_z are consecutive
-      Eigen::Vector4f vp_in(pstep[0], pstep[1], pstep[2], 1);
-      Eigen::Vector4f vp_out = transform * vp_in;
-
-      pstep[0] = vp_out[0];
-      pstep[1] = vp_out[1];
-      pstep[2] = vp_out[2];
-    }
-  }
-}
-
-
-void transformPointCloud(const std::string &target_frame,
-                         const geometry_msgs::Transform &net_transform,
-                         const sensor_msgs::PointCloud2 &in,
-                         sensor_msgs::PointCloud2 &out)
-{
-  if (in.header.frame_id == target_frame)
-  {
-    out = in;
-    return;
-  }
-
-  // Get the transformation
-  Eigen::Matrix4f transform;
-  transformAsMatrix(net_transform, transform);
-
-  transformPointCloud(transform, in, out);
-
-  out.header.frame_id = target_frame;
-}
 
 /*
  * Synchronized clouds callback
@@ -212,20 +82,10 @@ void quadCloudCallback(const perception_pipeline::QuadcloudConstPtr &quadcloud)
    */
   ROSPointCloud ros_cloud1_transformed, ros_cloud2_transformed, ros_cloud3_transformed, ros_cloud4_transformed;
 
-  // pcl_ros::transformPointCloud(world_frame, transformStamped1.transform, rcloud1, ros_cloud1_transformed);
-  // pcl_ros::transformPointCloud(world_frame, transformStamped2.transform, rcloud2, ros_cloud2_transformed);
-  // pcl_ros::transformPointCloud(world_frame, transformStamped3.transform, rcloud3, ros_cloud3_transformed);
-  // pcl_ros::transformPointCloud(world_frame, transformStamped4.transform, rcloud4, ros_cloud4_transformed);
-
-  transformPointCloud(world_frame, transformStamped1.transform, rcloud1, ros_cloud1_transformed);
-  transformPointCloud(world_frame, transformStamped2.transform, rcloud2, ros_cloud2_transformed);
-  transformPointCloud(world_frame, transformStamped3.transform, rcloud3, ros_cloud3_transformed);
-  transformPointCloud(world_frame, transformStamped4.transform, rcloud4, ros_cloud4_transformed);
-
-  // ros_cloud1_transformed.header.frame_id = "world_frame";
-  // ros_cloud2_transformed.header.frame_id = "world_frame";
-  // ros_cloud3_transformed.header.frame_id = "world_frame";
-  // ros_cloud4_transformed.header.frame_id = "world_frame";
+  pcl_ros::transformPointCloud(world_frame, transformStamped1.transform, rcloud1, ros_cloud1_transformed);
+  pcl_ros::transformPointCloud(world_frame, transformStamped2.transform, rcloud2, ros_cloud2_transformed);
+  pcl_ros::transformPointCloud(world_frame, transformStamped3.transform, rcloud3, ros_cloud3_transformed);
+  pcl_ros::transformPointCloud(world_frame, transformStamped4.transform, rcloud4, ros_cloud4_transformed);
 
   sensor1_pub.publish(ros_cloud1_transformed);
   sensor2_pub.publish(ros_cloud2_transformed);
@@ -392,20 +252,20 @@ int main(int argc, char *argv[])
 
   combined_pub = nh.advertise<ROSPointCloud>("env/combined/points", 1);
 
-  auto downsample_filter = priv_nh_.param<std::string>("downsample_filter", "none");
-  auto resize_filter = priv_nh_.param<std::string>("resize_filter", "none");
+  auto downsample_filter = nh.param<std::string>("downsample_filter", "none");
+  auto resize_filter = nh.param<std::string>("resize_filter", "none");
 
   if (downsample_filter == "octree")
   {
     enable_octreefilter = true;
     octreefilter_pub = nh.advertise<ROSPointCloud>("env/combined/octreefilter", 1);
-    voxel_leaf_size = priv_nh_.param<float>("voxel_leaf_size", 0.1);
+    voxel_leaf_size = nh.param<float>("voxel_leaf_size", 0.1);
   }
   else if (downsample_filter == "voxelgrid")
   {
     enable_voxelfilter = true;
     voxelfilter_pub = nh.advertise<ROSPointCloud>("env/combined/voxelfilter", 1);
-    voxel_leaf_size = priv_nh_.param<float>("voxel_leaf_size", 0.1);
+    voxel_leaf_size = nh.param<float>("voxel_leaf_size", 0.1);
   }
   else
   {
@@ -418,21 +278,21 @@ int main(int argc, char *argv[])
     enable_passfilter = true;
     passfilter_pub = nh.advertise<ROSPointCloud>("env/combined/passfilter", 1);
 
-    x_filter_min = priv_nh_.param<float>("x_filter_min", -2.5);
-    x_filter_max = priv_nh_.param<float>("x_filter_max", 2.5);
-    y_filter_min = priv_nh_.param<float>("y_filter_min", -2.5);
-    y_filter_max = priv_nh_.param<float>("y_filter_max", 2.5);
-    z_filter_min = priv_nh_.param<float>("z_filter_min", -2.5);
-    z_filter_max = priv_nh_.param<float>("z_filter_max", 2.5);
+    x_filter_min = nh.param<float>("x_filter_min", -2.5);
+    x_filter_max = nh.param<float>("x_filter_max", 2.5);
+    y_filter_min = nh.param<float>("y_filter_min", -2.5);
+    y_filter_max = nh.param<float>("y_filter_max", 2.5);
+    z_filter_min = nh.param<float>("z_filter_min", -2.5);
+    z_filter_max = nh.param<float>("z_filter_max", 2.5);
   }
   else
   {
     enable_passfilter = false;
   }
 
-  save_pcd = priv_nh_.param<bool>("save_pcd", false);
-  save_interval = priv_nh_.param<float>("save_interval", 2.0);
-  auto save_path = priv_nh_.param<std::string>("save_path", "output");
+  save_pcd = nh.param<bool>("save_pcd", false);
+  save_interval = nh.param<float>("save_interval", 2.0);
+  auto save_path = nh.param<std::string>("save_path", "output");
 
   save_path_full = ros::package::getPath("perception_pipeline") + "/" + save_path;
 
